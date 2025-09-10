89ced4aaf6371054453e19afccfff1ea1dd4441a
mohammedyaseer072 <mohammedyaseer072@gmail.com>
2025-09-10 09:48:15 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Hamburger menu toggle
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.querySelector("#navMenu ul");
-
-hamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
